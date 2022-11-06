98ec25cd6b206675c6d0d7da731e0a88fcf5e73d
watsy0007 <watsy0007@watsy0007MacMini.lan>
2022-11-06 16:19:57 +0800

    upgrade rustler

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,6 @@
-use Mix.Config
+import Config
 # config :blake3, simd_mode: "std"
 # config :blake3, rayon: :false
+config :blake3, Blake3.Native,
+  mode: :release,
+  features: MixBlake3.Project.config_features()
